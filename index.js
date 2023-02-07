d78f7df95e2f111876e43aeb5097bd85e0c24ab9
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-02-06 23:21:31 -0800

    added secondAnswer window.alert

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your Code here
+let firstAnswer = window.prompt("Do you like pineapples on pizza?")
+console.log(firstAnswer === "yes")
+
+//yes or no conditional statement
+if (firstAnswer === "yes") {
+    let secondAnswer = window.prompt("Is pizza your favorite food?")
+    }    
+    if (firstAnswer === "yes") {
+        let secondAnswer = window.alert("YOU ARE WINNING AT LIFE!!!")
+
+} else if(firstAnswer === "no") {
+    let secondAnswer = window.prompt("Do you like sardines on your pizza?")
+    }
+    if (firstAnswer === "yes") {
+        let secondAnswer = window.alert("GROSS!")
+
+}
